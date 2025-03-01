543efb80368fad7e3c4daa76d7ed51878ba7e563
TetsuoYagami <lvanchen749@gmail.com>
2025-02-28 21:27:38 -0700

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,15 @@
+var w = window.innerWidth;
+if (w <= 480){
+    document.getElementById("logo1").src = "Imagenes/Section 6.webp";
+}
+
+function Personajes(){
+    document.getElementById("Personaje").style.opacity = "1";
+    document.getElementById("Personaje").style.position = "static";
+
+
+}
+
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
